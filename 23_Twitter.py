7175329e56f6583df56a97164fb7c4e3682bc81e
GaneshSharma27 <ganesh27sharma09@gmail.com>
2024-01-05 18:28:16 +0530

    New concepts

--- 23_Twitter.py
+++ 23_Twitter.py
@@ -1,12 +1,16 @@
 import re
 
 url = input("URL: ").strip()
-# username = url.removeprefix("https://twitter.com/")
+# username = url.removeprefix("https://twitter.com/")   # `removeprefix()` removes the prefix of whatever is typed into its function arguments
 # print(f"Username: {username}")
-# # `removeprefix()` removes the prefix of whatever is typed into its function arguments
 
 # `sub()` stands for substitute
 # re.sub(pattern, repl, string, count=0, flags=0)
-username = re.sub(r"^((http|https)://)?(www\.)?twitter\.com/", "", url)
+# re.split(pattern, string, maxsplit=0, flags=0)
+# re.findall(pattern, string, flags=0)
+
+# username = re.sub(r"^((http|https)://)?(www\.)?twitter\.com/", "", url)
 #                       https? ---> also can be written like this
-print(f"Username: {username}")
+
+if (matches := re.search(r"^https://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE)):     # (?:www\.) ----> `?:` means don't consider it in a group
+    print(f"Username:", matches.group(1))
